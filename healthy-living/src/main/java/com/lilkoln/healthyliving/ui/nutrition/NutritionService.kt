9ad0918ed22513b94a982d2b59f7f3fb513e7d6b
kolyasuk <dzioba99@gmail.com>
2022-11-22 23:37:59 +0200

    Format amount

--- healthy-living/src/main/java/com/lilkoln/healthyliving/ui/nutrition/NutritionService.kt
+++ healthy-living/src/main/java/com/lilkoln/healthyliving/ui/nutrition/NutritionService.kt
@@ -6,6 +6,8 @@ import com.lilkoln.healthyliving.db.entity.Nutrition
 import com.lilkoln.healthyliving.db.entity.NutritionUnit
 import com.lilkoln.healthyliving.db.enums.FoodType
 import com.lilkoln.healthyliving.db.enums.MeasureUnit
+import java.math.BigDecimal
+import java.text.DecimalFormat
 import java.time.LocalDate
 import java.util.*
 
@@ -19,7 +21,7 @@ class NutritionService(private val mainActivityViewModel: MainActivityViewModel)
 
     suspend fun constructNutritionUnit(userCalories: Int, food: Food, percent: Int) {
         val foodPortionRate = getFoodAmount(userCalories, food.calories, percent)
-        val nutritionUnit = NutritionUnit(null, food.id, "%.2f".format(foodPortionRate * 100).toFloat(), MeasureUnit.CALORIES, false,
+        val nutritionUnit = NutritionUnit(null, food.id, foodPortionRate * 100, MeasureUnit.CALORIES, false,
             (foodPortionRate * food.calories).toInt(), (foodPortionRate * food.proteins).toInt())
 
         mainActivityViewModel.createNutritionUnit(nutritionUnit, food.type)
